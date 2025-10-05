0a456840593a7b28649ef91ed838ab7a7a9a9bac
claudihidal <claudihidal@gmail.com>
2025-10-04 22:47:56 -0500

    Flix: Cors Clase 11.

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,22 +1,33 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 import {
   CreateProductDTO,
   Product,
   UpdateProductDTO,
 } from './../models/product.model';
+import { retry } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductsService {
-  private apiUrl = 'https://young-sands-07814.herokuapp.com/api/products';
+  //private apiUrl = 'https://young-sands-07814.herokuapp.com/api/products';
+  private apiUrl = '/api/products';
 
   constructor(private http: HttpClient) {}
 
-  getAllProducts() {
-    return this.http.get<Product[]>(this.apiUrl);
+  getAllProducts(limit?: number, offset?: number) {
+    let params = new HttpParams();
+    if (limit && offset) {
+      params = params.set('limit', limit);
+      params = params.set('offset', limit);
+    }
+    return this.http.get<Product[]>(this.apiUrl)
+    .pipe(
+      retry(3)
+    );
+
     //return this.http.get<Product[]>('https://fakestoreapi.com/products');
   }
 
